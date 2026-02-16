02c27647db639178a4459b5feb42d45ae3066ef1
SachinB <sachin1214bhatt@gmail.com>
2026-02-16 04:14:35 -0600

    Setup react-router-dom for pages. setup header and footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import ProductsPage from "./pages/ProductsPage";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Cart from "./components/cart/Cart";
+
+function AppLayout() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductDetailPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>
-    <HomePage/>
-  </div>;
+  return <RouterProvider router={router} />;
 }
 export default App;
