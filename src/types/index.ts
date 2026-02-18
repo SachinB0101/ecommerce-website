7f775db4e208a3b8a58fc118dc5cbd8c09135cc4
SachinB <sachin1214bhatt@gmail.com>
2026-02-17 22:28:09 -0600

    Add more types

--- src/types/index.ts
+++ src/types/index.ts
@@ -28,3 +28,37 @@ export interface CartItem {
   size?: string;
   color?: string;
 }
+
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  imageUrl?: string;
+}
+
+export interface Address {
+  id: string;
+  fullName: string;
+  addressLine1: string;
+  addressLine2?: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+  isDefault: boolean;
+}
+
+export interface PaymentMethod {
+  id: string;
+  cardType: string;
+  lastFour: string;
+  expiryMonth: string;
+  expiryYear: string;
+  isDefault: boolean;
+}
+
+export interface WishlistItem {
+  productId: string;
+  addedAt: string;
+}
