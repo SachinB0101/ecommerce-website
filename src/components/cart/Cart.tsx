f5769d22bc2073afe7c67b94229139f1ba71d1e3
SachinB <sachin1214bhatt@gmail.com>
2026-02-21 04:30:00 -0600

    Create a simple sheet for cart, which opens when clicked

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,5 +1,25 @@
+import { useAppDispatch, useAppSelector } from "@/app/hooks/useRedux";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { setCartOpen } from "@/features/cart/cartSlice";
+
 export const Cart = () => {
+  const dispatch = useAppDispatch();
+  const { isOpen } = useAppSelector((state) => state.cart);
+
   return (
-    <div>Cart</div>
-  )
-}
+    <Sheet open={isOpen} onOpenChange={(open) => dispatch(setCartOpen(open))}>
+      <SheetContent className="flex flex-col w-full sm:max-w-lg">
+        <SheetHeader>
+          <SheetTitle className="font-display text-2xl">
+            Shopping Bag (WORKING)
+          </SheetTitle>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
