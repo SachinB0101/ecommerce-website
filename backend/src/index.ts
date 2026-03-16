f4027cd78030ccc30d0d676b00210a3782995a1d
SachinB <sachin1214bhatt@gmail.com>
2026-03-16 18:52:12 -0500

    Add a health checkout route for my server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use("/api/payments", paymentRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`),
